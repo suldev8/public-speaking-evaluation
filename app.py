16ee80e05b80469b5503b110d97d96103411108f
Sulaiman Alhabib <suldev8@gmail.com>
2019-03-04 19:20:41 +0300

    Fix formating

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ style.use('fivethirtyeight')
 
 y_pos = np.arange(len(emotions))
 
+
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -43,7 +44,7 @@ class App(tk.Tk):
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
-        #show the passed page class
+        # show the passed page class
         frame = self.frames[cont]
         frame.tkraise()
 
@@ -51,24 +52,25 @@ class App(tk.Tk):
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        logo = ImageTk.PhotoImage(Image.open("assets/app_icon.png"), master=self)
+        logo = ImageTk.PhotoImage(Image.open(
+            "assets/app_icon.png"), master=self)
         logo_label = ttk.Label(self, image=logo)
         logo_label.image = logo
         logo_label.pack()
 
-        #Label to explain how to use the buttons
+        # Label to explain how to use the buttons
         start_label = ttk.Label(self, text="Load a video or click stream to analyze emotions",
                                 font=LARGE_FONT)
         start_label.pack(pady=10, padx=10)
         btn_frame = ttk.Frame(self)
         btn_frame.pack()
 
-        #Load button to load a video
+        # Load button to load a video
         load_btn = ttk.Button(btn_frame, text="Load Video",
                               command=lambda: self.load_video(controller))
         load_btn.pack(pady=10, padx=10, side="left")
 
-        #Stream button to run the webcam
+        # Stream button to run the webcam
         stream_btn = ttk.Button(btn_frame, text="Streaming",
                                 command=lambda: self.streaming(controller))
         stream_btn.pack(pady=10, padx=10, side="left")
@@ -86,6 +88,7 @@ class StartPage(tk.Frame):
 
         if video_path:
             controller.show_frame(EmotionRecognitionPage)
+
     def streaming(self, controller):
         global video_path
         video_path = 0
@@ -94,46 +97,47 @@ class StartPage(tk.Frame):
 
 class EmotionRecognitionPage(tk.Frame):
     def __init__(self, parent, controller):
-        
+
         tk.Frame.__init__(self, parent)
         btn_frame = ttk.Frame(self)
         btn_frame.pack(side="top")
-        
-        #Back button to go back to the StartPage
+
+        # Back button to go back to the StartPage
         back_btn = ttk.Button(
             btn_frame, text="Back", command=lambda: self.back_to_start_page(controller))
         back_btn.pack(pady=10, padx=10, side="left")
-        
-        #Start/Stop button  to start/stop analyzing
+
+        # Start/Stop button  to start/stop analyzing
         self.start_stop_btn = ttk.Button(
             btn_frame, text="Start", command=self.start_stop)
         self.start_stop_btn.pack(pady=10, padx=10, side="right")
-        
-        #initalize the video variable and create its frame and label
+
+        # initalize the video variable and create its frame and label
         self.video = None
         self.video_frame = tk.Frame(self)
         self.video_frame.pack()
         self.video_label = tk.Label(self.video_frame)
-        
-        #creating and setting up the bar graph
+
+        # creating and setting up the bar graph
         self.fig = Figure()
         self.ax = self.fig.add_subplot(111)
         self.fig.subplots_adjust(left=0.10)
-        self.bars = self.ax.bar(y_pos, np.zeros(len(emotions)), align='center', alpha=0.5)
+        self.bars = self.ax.bar(y_pos, np.zeros(
+            len(emotions)), align='center', alpha=0.5)
         self.ax.set_ylabel('percentage')
         self.ax.set_title('emotion')
         self.ax.set_xticks(y_pos)
         self.ax.set_xticklabels(emotions)
         self.ax.set_yticks(np.arange(0, 110, 10))
-        
+
         self.canvas_bar = FigureCanvasTkAgg(self.fig, self.video_frame)
         self.canvas_bar.draw()
-        
-        #pregress bar for loading the video and analyzing it
-        self.progress_bar = ttk.Progressbar(self.video_frame, orient='horizontal', length=270, mode='determinate')
-        
 
-        #average of emotion to count all predictions and devide on number of frames
+        # pregress bar for loading the video and analyzing it
+        self.progress_bar = ttk.Progressbar(
+            self.video_frame, orient='horizontal', length=270, mode='determinate')
+
+        # average of emotion to count all predictions and devide on number of frames
         self.average_emotion = np.zeros(len(emotions))
         self.numb_of_frames = 0
 
@@ -142,16 +146,17 @@ class EmotionRecognitionPage(tk.Frame):
             self.start_stop_btn.config(text="Stop")
             if video_path == 0:
                 self.video_label.pack(pady=10, padx=10, side="left")
-                self.canvas_bar.get_tk_widget().pack(pady=10, padx=10, side="right", fill="both", expand="1")
+                self.canvas_bar.get_tk_widget().pack(
+                    pady=10, padx=10, side="right", fill="both", expand="1")
             self.start_video()
-        
+
         elif self.start_stop_btn.cget("text") == "Stop":
             self.video.__del__()
             self.start_stop_btn.config(text="Start")
             self.video_label.forget()
             self.canvas_bar.get_tk_widget().forget()
             self.show_average()
-    
+
     def show_average(self):
         self.canvas_bar.get_tk_widget().pack(pady=10, padx=10, side="top")
         self.average_emotion /= self.numb_of_frames
@@ -167,13 +172,14 @@ class EmotionRecognitionPage(tk.Frame):
             self.stream_video()
 
     def stream_video(self):
-        #read the video from the streaming camera and analyze it
+        # read the video from the streaming camera and analyze it
         ret, frame = self.video.get_frame()
         if ret:
             frame, emotion_predictions = get_emotion_predictions(frame)
-            
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image = ImageTk.PhotoImage(image = Image.fromarray(frame), master=self.video_frame)
+            image = ImageTk.PhotoImage(
+                image=Image.fromarray(frame), master=self.video_frame)
             self.video_label.imgtk = image
             self.video_label.config(image=image)
             if(emotion_predictions.all(0)):
@@ -181,55 +187,29 @@ class EmotionRecognitionPage(tk.Frame):
                 self.numb_of_frames += 1
             self.draw_bar_chart(emotion_predictions)
             self.video_label.after(100, self.stream_video)
-            
+
         else:
             self.video_label.forget()
-        
+
     def load_video(self):
-        """#get the number of frames in the video
+        # get the number of frames in the video
         count_frames = int(self.video.count_frames())
-        print(count_frames)
-        print(video_path)
         self.progress_bar.config(maximum=count_frames)
-        
-        #self.progress_bar.start()
-        #loop through frames and analyze it
-        while True:
-            ret, frame = self.video.get_frame()
-            print(ret)
-            print(type(frame))
-            if ret:
-                frame, emotion_predictions = get_emotion_predictions(frame)
-                if(emotion_predictions.all(0)):
-                    self.average_emotion += emotion_predictions
-                    self.numb_of_frames += 1
-                self.progress_bar.step(1)
-            else:
-                print("break the loop")
-                break
-        self.progress_bar.forget() """
-        #get the number of frames in the video
-        count_frames = int(self.video.count_frames())
-        print(count_frames)
-        print(video_path)
-        self.progress_bar.config(maximum=count_frames)
-        
-        #loop through frames and analyze it
+
+        # read video and analyze it
         ret, frame = self.video.get_frame()
-        print(ret)
-        print(type(frame))
         if ret:
             frame, emotion_predictions = get_emotion_predictions(frame)
             if(emotion_predictions.all(0)):
                 self.average_emotion += emotion_predictions
                 self.numb_of_frames += 1
             self.progress_bar.step(1)
-            self.progress_bar.after(1,self.load_video)
+            self.progress_bar.after(1, self.load_video)
         else:
-            #remove progress barstop loading and 
+            # remove progress barstop loading and
             self.progress_bar.forget()
             self.start_stop()
-    
+
     def draw_bar_chart(self, emotion_predictions):
 
         emotion_predictions *= 100
@@ -245,11 +225,13 @@ class EmotionRecognitionPage(tk.Frame):
         self.video_label.forget()
         controller.show_frame(StartPage)
 
+
 class VideoCapture:
     def __init__(self, video_path=0):
         self.video = cv2.VideoCapture(video_path)
         if not self.video.isOpened():
             raise ValueError("Couldn't open video file", video_path)
+
     def get_frame(self):
         if self.video.isOpened():
             ret, frame = self.video.read()
@@ -259,12 +241,14 @@ class VideoCapture:
                 return (ret, None)
         else:
             return(False, None)
+
     def count_frames(self):
         return self.video.get(cv2.CAP_PROP_FRAME_COUNT)
-    
+
     def __del__(self):
         if self.video.isOpened():
             self.video.release()
+
 
 if __name__ == "__main__":
     App().mainloop()
